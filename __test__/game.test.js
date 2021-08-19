8c85b1e95c2a12c637c5208b0dccfa8ed5c11932
Steve Miller <smiller@crowninformationservices.com>
2021-08-19 15:50:47 -0500

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -2,7 +2,7 @@ const Game = require('../src/game').default
 const fs = require('fs')
 
 describe('App', () => {
-  it('Contains the compiled JavaScript', async (done) => {
+  it('Contains the compiled JavaScript', done => {
     fs.readFile('./public/main.js', 'utf8', (err, data) => {
       expect(err).toBe(null)
       expect(data).toMatchSnapshot()
